5eb2febc0887dba1d22634ba3f9a9a007364d519
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-20 23:18:07 +0300

    test: add RED immutability invariant

---
+++ tests/Mizan.Domain.Tests/ImmutabilityTests.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+using Mizan.Domain.Finance;
+
+namespace Mizan.Domain.Tests;
+
+public sealed class ImmutabilityTests
+{
+    [Fact]
+    public void Accepted_operation_effects_should_not_be_mutable()
+    {
+        var account = Account.Create("Cash", AccountType.Cash, "EGP");
+        var operation = FinancialOperation.Income(
+            account.Id,
+            Money.FromMinorUnits(100_00, "EGP"),
+            DateTimeOffset.UtcNow).Accept();
+
+        operation.IsImmutable.Should().BeTrue();
+        operation.Effects.Should().BeReadOnly();
+    }
+}
