28e5c2db2382cbef19bd9b3853d73939809b2718
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-21 00:39:25 +0300

    fix: import EF migration attribute types

--- src/Mizan.Infrastructure/Migrations/20260921230000_AddReversalLinkage.Designer.cs
+++ src/Mizan.Infrastructure/Migrations/20260921230000_AddReversalLinkage.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mizan.Infrastructure.Persistence;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
