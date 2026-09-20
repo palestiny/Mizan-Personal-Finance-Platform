4cbdaabd58b67bcec7795f3fa03515877922d5fa
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-21 00:23:50 +0300

    fix: correct reversal index filter

--- src/Mizan.Infrastructure/Migrations/MizanDbContextModelSnapshot.cs
+++ src/Mizan.Infrastructure/Migrations/MizanDbContextModelSnapshot.cs
@@ -70,7 +70,7 @@ namespace Mizan.Infrastructure.Migrations
                 b.Property<DateTimeOffset>("RecordedAt").HasColumnType("timestamp with time zone");
                 b.Property<int>("Type").HasColumnType("integer");
                 b.HasKey("Id");
-                b.HasIndex("OriginalOperationId").IsUnique().HasFilter("\"original_operation_id\" IS NOT NULL");
+                b.HasIndex("OriginalOperationId").IsUnique().HasFilter("\"OriginalOperationId\" IS NOT NULL");
                 b.ToTable("financial_operations", (string)null);
             });
 
