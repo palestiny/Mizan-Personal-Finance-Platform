ba96a7cb4993accee21a049dd1e8ea2e352c7478
palestiny <153906782+palestiny@users.noreply.github.com>
2026-09-20 23:18:03 +0300

    test: add RED money invariants

---
+++ tests/Mizan.Domain.Tests/MoneyTests.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+using Mizan.Domain.Finance;
+
+namespace Mizan.Domain.Tests;
+
+public sealed class MoneyTests
+{
+    [Fact]
+    public void Money_should_store_minor_units_exactly()
+    {
+        var money = Money.FromMinorUnits(12345, "EGP");
+
+        money.MinorUnits.Should().Be(12345);
+        money.Currency.Should().Be("EGP");
+    }
+
+    [Fact]
+    public void Money_should_not_use_floating_point_for_authoritative_value()
+    {
+        var money = Money.FromMinorUnits(999, "EGP");
+
+        money.MinorUnits.Should().BeOfType<long>();
+    }
+}
